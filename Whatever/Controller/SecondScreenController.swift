5e6cec65b30bffc9532937b85961697cc1f3d852
SirB31415 <31518716+SirB31415@users.noreply.github.com>
2017-10-12 13:11:16 -0600

    Added a new view controller for a second screen on the story board and a controller

---
+++ Whatever/Controller/SecondScreenController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondScreenController.swift
+//  Whatever
+//
+//  Created by Brailow, Parker on 10/12/17.
+//  Copyright © 2017 Brailow Inc. All rights reserved.
+//
+
+import UIKit
+
+public class SecondStringController : UIViewController
+{
+    
+}
